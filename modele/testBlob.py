24d9265847a39a4a57270959155e9331b72fefe8
Roméo <64222721+Pikpoukoe@users.noreply.github.com>
2023-07-17 11:43:24 +0300

    config d'un blob mais sa bug sa grandmere

--- modele/testBlob.py
+++ modele/testBlob.py
@@ -1,29 +1,39 @@
 # Standard imports
 import cv2
-import numpy as np;
+import numpy as np
 
-while(True):
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    # Set up the detector with parameters
+    params = cv2.SimpleBlobDetector_Params()
+    params.filterByArea = True
+    params.minArea = 1000
+    params.filterByCircularity = True
+    params.minCircularity = 0.9
+
+    detector = cv2.SimpleBlobDetector_create(params)
+
+    # Webcam
+    ret, im = cap.read()
     # Read image
-    im = cv2.imread("assets/circles.jpg")
-    
-    # Set up the detector with default parameters
-    detector = cv2.SimpleBlobDetector()
-    
-    # Detect blobs.
+    #im = cv2.imread("assets/circles.jpg")
+
+    # Detect blobs
     keypoints = detector.detect(im)
-    
-    # Draw detected blobs as red circles.
+
+    # Draw detected blobs as red circles
     # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-    #im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    
+    im_with_keypoints = cv2.drawKeypoints(im, [kp.pt for kp in keypoints], np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
     # Show keypoints
-    cv2.imshow("Keypoints", keypoints)
+    cv2.imshow("Blobs", im_with_keypoints)
     cv2.waitKey(0)
 
     # Quitter la boucle si 'q' est pressé
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(0) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
-#im.release()
+im.release()
 cv2.destroyAllWindows()
