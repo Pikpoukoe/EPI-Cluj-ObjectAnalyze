437ce9a8575141f5ccfd3eb87b6772eb7b2094c2
flodlb <florianbdlb2@gmail.com>
2023-07-31 16:17:32 +0300

    essai de la detection du rectangle

--- modele/dectectionRond.py
+++ modele/dectectionRond.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
-video_capture = cv2.VideoCapture(0)  # Indice 0 pour la caméra par défaut
 
+video_capture = cv2.VideoCapture(0)  # Indice 0 pour la caméra par défaut
 distance_objet = 15  # Distance en cm
 rayon_objet = 0  # Initialisation du rayon de l'objet
 
@@ -15,6 +15,7 @@ while True:
     # Détecter les contours
     edges = cv2.Canny(blurred, 50, 150)
     circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 1, 50, param1=200, param2=30, minRadius=5, maxRadius=100)
+
     # S'il y a des cercles détectés, mesurer le diamètre de l'objet et montre le cercle
     if circles is not None:
         circles = np.round(circles[0, :]).astype("int")
@@ -25,22 +26,18 @@ while True:
             cv2.circle(frame, (x, y), 2, (0, 0, 255), 3)
 
             # Calculer le diamètre de l'objet en pixels
-            diametre_pixels = 2 * y
-
+            diametre_pixels = 2 * r
             # Convertir le diamètre en cm en utilisant la relation de taille connue
             diametre_cm = (diametre_pixels / 38)
-
-
             # Afficher le diamètre à côté de l'objet
             cv2.putText(frame, f"Diametre: {diametre_cm:.2f} cm", (x - 70, y - r-15), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0, 255, 0), 2)
 
+
     # Afficher l'image avec les cercles détectés et le diamètre
     cv2.imshow("Objet rond", frame)
-
     # Quitter la boucle si la touche 'q' est pressée
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
-
 # Libérer les ressources
 video_capture.release()
 cv2.destroyAllWindows()
