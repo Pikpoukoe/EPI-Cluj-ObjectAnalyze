6848025fc02dcbd186eca9401218793ac65853a9
Roméo <64222721+Pikpoukoe@users.noreply.github.com>
2023-08-02 11:48:29 +0300

    update UI w/ pause button

--- MVC/WebcamControleur.py
+++ MVC/WebcamControleur.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import cv2
 
+
 class WebcamModele:
     def __init__(self, video_source=0):
         self.vid = cv2.VideoCapture(video_source)
@@ -22,6 +23,15 @@ class WebcamModele:
         if self.vid.isOpened():
             self.vid.release()
 
+    def button_pause_clicked(self):
+        WebcamModele.__init__(self)
+        success, image = WebcamModele.get_frame(self)
+        frame_count = 0
+        while success:
+            cv2.imwrite(f"assets/frame_{frame_count}.jpg", image)
+            success, image = WebcamModele.get_frame(self)
+            frame_count += 1
+
 
 class WebcamVue:
     def __init__(self, root, width=600, height=480):
@@ -33,13 +43,21 @@ class WebcamVue:
         # set maximum window size value
         root.maxsize(1200, 800)
 
-    def frame_left_right(self):
         # Create left and right frames
-        left_frame = tk.Frame(root, width=200, height=400, bg='grey')
-        left_frame.pack(side='left',  fill='both',  padx=10,  pady=5,  expand=True)
+        self.left_frame = tk.Frame(root, width=500, height=400, bg='lightgrey')
+        self.left_frame.pack(side='left', fill='both', padx=10, pady=5, expand=True)
 
-        right_frame = tk.Frame(root,  width=650,  height=400,  bg='grey')
-        right_frame.pack(side='right',  fill='both',  padx=10,  pady=5,  expand=True)
+        self.right_frame = tk.Frame(root, width=500, height=400, bg='lightgrey')
+        self.right_frame.pack(side='right', fill='both', padx=10, pady=5, expand=True)
+
+        self.tool_bar = tk.Frame(self.left_frame, width=50, height=50, bg='grey')
+        self.tool_bar.pack(side='top', fill='both', padx=5, pady=5, expand=False)
+
+    def clicked(self):
+        print("Click")
+
+    def button_pause_view(self):
+        tk.Button(self.tool_bar, text="Pause", command=WebcamModele.button_pause_clicked(self)).pack(padx=5, pady=5)
 
 
 class WebcamControleur:
@@ -49,7 +67,6 @@ class WebcamControleur:
         self.modele = WebcamModele()
         self.vue = WebcamVue(root)
         self.root.bind('<q>', self.close_windows)
-        self.vue.frame_left_right()
 
         # After it is called once, the update method will be automatically called every delay milliseconds
         self.delay = 15
@@ -61,7 +78,6 @@ class WebcamControleur:
         if ret:
             self.photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
             self.vue.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
-
         self.root.after(self.delay, self.update)
 
     def close_windows(self, event=None):
